a3abcedd5668df2491713373a644d2a99fbcee15
Abhishek Biswas <35541577+bisu85@users.noreply.github.com>
2023-10-11 15:51:17 +0200

    adding waitress serve for production server. + env

--- GreetAI.py
+++ GreetAI.py
@@ -1,3 +1,4 @@
+from waitress import serve
 from flask import Flask, render_template, request, session
 from flask_wtf import FlaskForm
 
@@ -250,4 +251,5 @@ if __name__ == '__main__':
 
     with ApiClient(configuration) as api_client:
         signature_request_api = apis.SignatureRequestApi(api_client)
-        app.run()
+        #app.run()
+        serve(app, host='127.0.0.1', port=8000)
